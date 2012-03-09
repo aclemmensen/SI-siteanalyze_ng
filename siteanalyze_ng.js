1695b086b0c954384059b1c4aab0d57613cb0158
Asbjørn Clemmensen <ac@siteimprove.com>
2012-03-09 13:00:56 +0100

    Loader feedback-script hvis _sz.push(['feedback', null]) bliver kaldt.

--- siteanalyze_ng.js
+++ siteanalyze_ng.js
@@ -217,6 +217,13 @@
 			}
 		},
 
+		'feedback': function(args) {
+			console.log('loading feedback');
+			var szfb = document.createElement('script'); szfb.type = 'text/javascript'; szfb.async = true;
+			szfb.src = '//ac.givetwise.dk/siteanalyze_fb/feedback.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(szfb, s);
+		},
+
 		'callbacks': {
 			'load':    null,
 			'request': null
